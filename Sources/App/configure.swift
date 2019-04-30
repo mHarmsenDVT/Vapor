9d0b184b3091b7e09854bd0b2dbfd7232deba80a
Marie Kristein-Harmsen <mharmsen.dvt@gmail.com>
2019-04-30 11:03:56 +0200

    Server side swift localhost:8080/hello

---
+++ Sources/App/configure.swift
@@ -0,0 +1,25 @@
+import Vapor
+import Leaf
+
+/// Called before your application initializes.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#configureswift)
+public func configure(
+    _ config: inout Config,
+    _ env: inout Environment,
+    _ services: inout Services
+) throws {
+    // Register routes to the router
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+    try services.register(LeafProvider())
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
+    // Configure the rest of your application here
+    /// Register middleware
+    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    services.register(middlewares)
+
+}
---
+++ Sources/App/routes.swift
@@ -0,0 +1,12 @@
+import Routing
+import Vapor
+import Leaf
+
+/// Register your application's routes here.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
+public func routes(_ router: Router) throws {
+    router.get("hello") { req -> Future<View> in
+        return try req.view().render("index")
+    }
+}
